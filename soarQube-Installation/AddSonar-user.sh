542ed8f791f952e4b709ba9be9458c57c04530bb
George Ebeh <114604124+georgeebeh@users.noreply.github.com>
2022-12-12 11:25:42 +0000

    Create AddSonar-user.sh

---
+++ soarQube-Installation/AddSonar-user.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Author: Landmark Technology
+# CentOs or REHEL 7/8
+
+#As a good security practice, SonarQuber Server is not advised to run sonar service as a root user, 
+#so create a new user called sonar and grant sudo access to manage sonar services as follows
+# 1. create a sonar 
+sudo useradd sonar
+# 2. Grand sudo access to sonar user
+sudo echo "sonar ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/sonar
+sudo chown -R sonar:sonar /opt/sonarqube/
+sudo chmod -R 775 /opt/sonarqube/
+hostname sonar
+sudo su - sonar
